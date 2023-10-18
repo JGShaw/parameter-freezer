1b94d71889dadcbc99f8b05beb790eb3adb5e596
Joseph Shaw <j.g.shaw95@gmail.com>
2023-10-18 16:41:20 +0100

    WIP: Remove localStorage in popup js

--- popup/popup.js
+++ popup/popup.js
@@ -1,15 +1,19 @@
 function freeze(obj) {
     const key = obj.currentTarget.paramKey
     const value = obj.currentTarget.paramValue
-    localStorage.setItem(key, value)
-    redraw()
+    chrome.storage.local.set({[key]: value}, () => {
+        console.log("Set param")
+        redraw()
+    });
 }
 
 function unfreeze(obj) {
     const key = obj.currentTarget.paramKey
     const value = obj.currentTarget.paramValue
-    localStorage.removeItem(key)
-    redraw()
+    chrome.storage.local.remove(key, () => {
+        console.log("Removed param")
+        redraw()
+    });
 }
 
 function redraw() {
@@ -17,53 +21,58 @@ function redraw() {
     $("#unfrozenTable").empty(); 
 
 
-    const frozenParams = [];
+    chrome.storage.local.get(null, function (items) {
+        
+        const frozenParams = []
 
-    for (const [key, value] of Object.entries(localStorage)) {
-        $('#frozenTable').append($(`<tr><td>${key}</td><td>${value}</td><td><input type="button" value="unfreeze" id="${key}=${value}" /></td></tr>`))
-        const button = document.getElementById(key + "=" + value)
-        button.addEventListener("click", unfreeze, false)
-        button.paramKey = key
-        button.paramValue = value
-        frozenParams.push({"key": key, "value": value});
-    }
-
-
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        const searchParams = new URL(tabs[0].url).searchParams;
-        for (const [key, value] of searchParams) {
-            if(!localStorage.getItem(key)) {
-                $('#unfrozenTable').append($(`<tr><td>${key}</td><td>${value}</td><td><input type="button" value="freeze" id="${key}=${value}" /></td></tr>`))
-                const button = document.getElementById(key + "=" + value)
-                button.addEventListener("click", freeze, false)
-                button.paramKey = key
-                button.paramValue = value
-            }
+        for (const [key, value] of Object.entries(items)) {
+            $('#frozenTable').append($(`<tr><td>${key}</td><td>${value}</td><td><input type="button" value="unfreeze" id="${key}=${value}" /></td></tr>`))
+            const button = document.getElementById(key + "=" + value)
+            button.addEventListener("click", unfreeze, false)
+            button.paramKey = key
+            button.paramValue = value
+            frozenParams.push({"key": key, "value": value});
         }
-    });
 
-    chrome.declarativeNetRequest.getDynamicRules(previousRules => {
-        chrome.declarativeNetRequest.updateDynamicRules({
-            removeRuleIds: [1],
-            addRules: [{
-                "id": 1,
-                "action": {
-                    "type": "redirect",
-                    "redirect": {
-                        "transform": {
-                            "queryTransform": {
-                                "addOrReplaceParams": frozenParams
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            const searchParams = new URL(tabs[0].url).searchParams;
+            for (const [key, value] of searchParams) {
+                chrome.storage.local.get(key, function(obj) {
+                    if(JSON.stringify(obj) === '{}') {
+                        $('#unfrozenTable').append($(`<tr><td>${key}</td><td>${value}</td><td><input type="button" value="freeze" id="${key}=${value}" /></td></tr>`))
+                        const button = document.getElementById(key + "=" + value)
+                        button.addEventListener("click", freeze, false)
+                        button.paramKey = key
+                        button.paramValue = value
+                    }
+
+                });        
+            }
+        });
+
+        chrome.declarativeNetRequest.getDynamicRules(previousRules => {
+            chrome.declarativeNetRequest.updateDynamicRules({
+                removeRuleIds: [1],
+                addRules: [{
+                    "id": 1,
+                    "action": {
+                        "type": "redirect",
+                        "redirect": {
+                            "transform": {
+                                "queryTransform": {
+                                    "addOrReplaceParams": frozenParams
+                                }
                             }
                         }
+                    },
+                    "condition": {
+                        "urlFilter": "*://*/*",
+                        "resourceTypes" : ["main_frame"]
                     }
-                },
-                "condition": {
-                    "urlFilter": "*://*/*",
-                    "resourceTypes" : ["main_frame"]
-                }
-            }]
+                }]
+            });
         });
-    }), () => { console.log("Updated rules")};
+    });
 }
 
 redraw()
--- service_worker.js
+++ service_worker.js
@@ -1,6 +1,36 @@
-chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((e) => {
-  const msg = `Navigation blocked to ${e.request.url} on tab ${e.request.tabId}.`;
-  console.log(msg);
-});
+// function onUpdated(tab) {
+  // console.log(`Updated tab: ${tab.id}`);
+// }
+
+// function onError(error) {
+  // console.log(`Error: ${error}`);
+// }
+// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    // console.log("url change listener")
+    // if (changeInfo.url) {
+        // const url = new URL(changeInfo.url);
+        // const searchParams = url.searchParams;
+
+        // const paramsToAdd = []
+        // for (const [key, value] of Object.entries(localStorage)) {
+            // if(!searchParams[key]) {
+                // paramsToAdd.push(key);
+            // }
+        // }
+
+        // console.log("paramsToAdd", paramsToAdd);
+
+        // if(paramsToAdd.length > 0) {
+            // for (const key of paramsToAdd) {
+                // url.searchParams.set(key, localStorage.getItem(key))
+            // }
+
+            // chrome.tabs.update(tabId, {
+                // url: url.toString()
+            // }).then(onUpdated, onError);
+
+        // }
+    // }
+// });
 
 console.log('Service worker started.');
