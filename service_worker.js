9dfd365af3153d1296810ad7202fa2a213359a1c
Joseph Shaw <j.g.shaw95@gmail.com>
2023-10-18 16:41:20 +0100

    Add background worker to redirect when params are not present

--- service_worker.js
+++ service_worker.js
@@ -1,36 +1,31 @@
-// function onUpdated(tab) {
-  // console.log(`Updated tab: ${tab.id}`);
-// }
+function onUpdated(tab) {
+  console.log(`Updated tab: ${tab.id}`);
+}
 
-// function onError(error) {
-  // console.log(`Error: ${error}`);
-// }
-// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    // console.log("url change listener")
-    // if (changeInfo.url) {
-        // const url = new URL(changeInfo.url);
-        // const searchParams = url.searchParams;
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
 
-        // const paramsToAdd = []
-        // for (const [key, value] of Object.entries(localStorage)) {
-            // if(!searchParams[key]) {
-                // paramsToAdd.push(key);
-            // }
-        // }
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.url) {
+        const url = new URL(changeInfo.url);
+        const searchParams = url.searchParams;
 
-        // console.log("paramsToAdd", paramsToAdd);
+        chrome.storage.local.get(null, function (items) {
+            redirect = false
 
-        // if(paramsToAdd.length > 0) {
-            // for (const key of paramsToAdd) {
-                // url.searchParams.set(key, localStorage.getItem(key))
-            // }
-
-            // chrome.tabs.update(tabId, {
-                // url: url.toString()
-            // }).then(onUpdated, onError);
-
-        // }
-    // }
-// });
+            for (const [key, value] of Object.entries(items)) {
+                if(!searchParams[key]) {
+                    console.log("Adding", key, value)
+                    url.searchParams.set(key, value);
+                    redirect = true
+                }
+            }
+            if(redirect) {
+                chrome.tabs.update(tabId, { url: url.toString() }).then(onUpdated, onError);
+            }
+        });
+    }
+});
 
 console.log('Service worker started.');
